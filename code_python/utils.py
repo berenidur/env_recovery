a707454e7c44c9fae571c65bf964a3273b36dd0b
berenidur <rodolfohuacasi@gmail.com>
2025-03-03 19:21:06 -0500

    Add functions for calculating and retrieving windows from HDF5 datasets

--- code_python/utils.py
+++ code_python/utils.py
@@ -32,10 +32,60 @@ def disp_time(seconds):
 
 def plot_losses(history, epoch, modelname):
     plt.figure(figsize=(8, 6))
-    plt.plot(history['train_loss'], label='Train Loss')
-    plt.plot(history['val_loss'], label='Validation Loss')
+    plt.plot(range(1, epoch + 1), history['train_loss'], label='Train Loss')
+    plt.plot(range(1, epoch + 1), history['val_loss'], label='Validation Loss')
+    plt.grid(True)
+    plt.xlim(1, epoch)
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.title(f'Loss after {epoch} epochs')
     plt.legend()
     plt.savefig('imgs/'+modelname+'_loss.png')
+
+
+def calculate_total_windows_h5(h5_path, n):
+    total_windows = 0
+    with h5py.File(h5_path, 'r') as file:
+        for group in file.keys():
+            dataset_name = f'{group}/comp_env_interp_1'
+            if dataset_name in file:
+                data = np.array(file[dataset_name])
+                if data.ndim == 2:
+                    num_windows = ((data.shape[0] - n + 1) * (data.shape[1] - n + 1))
+                    total_windows += num_windows
+                else:
+                    raise ValueError(f"Dataset {dataset_name} is not 2-dimensional")
+            else:
+                raise ValueError(f"Dataset {dataset_name} not found")
+    return total_windows
+
+def get_window_xy_h5(h5_path, n, window_idx):
+    total_windows = calculate_total_windows_h5(h5_path, n)
+    if window_idx >= total_windows:
+        raise IndexError("Window index out of range")
+    
+    current_window = 0
+    with h5py.File(h5_path, 'r') as file:
+        for group in file.keys():
+            dataset_name = f'{group}/comp_env_interp_1'
+            valid_rs_name = f'{group}/validRS'
+            if dataset_name in file and valid_rs_name in file:
+                data = np.array(file[dataset_name])
+                if data.ndim == 2:
+                    num_windows = ((data.shape[0] - n + 1) * (data.shape[1] - n + 1))
+                    if current_window + num_windows > window_idx:
+                        local_idx = window_idx - current_window
+                        row_idx = local_idx // (data.shape[1] - n + 1)
+                        col_idx = local_idx % (data.shape[1] - n + 1)
+                        comp_env_window = data[row_idx:row_idx+n, col_idx:col_idx+n]
+                        
+                        valid_rs = np.array(file[valid_rs_name])
+                        valid_rs_value = valid_rs[row_idx, col_idx]
+
+                        return comp_env_window, valid_rs_value
+                    current_window += num_windows
+                else:
+                    raise ValueError(f"Dataset {dataset_name} is not 2-dimensional")
+            else:
+                raise ValueError(f"Dataset {dataset_name} or {valid_rs_name} not found")
+    raise IndexError("Window index out of range")
